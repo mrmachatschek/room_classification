da480c29bef0608b7b346e91b79e6f8fad195eee
Michael Machatschek <m20190054@novaims.unl.pt>
2020-05-29 09:54:24 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+
+# Part 0 - File structure into df
+
+import os
+import pandas as pd
+
+#https://stackoverflow.com/questions/42654961/creating-pandas-dataframe-from-os
+res = []
+for root, dirs, files in os.walk('E:\\Dados\\FLH HOLIDAY RENTALS\\011_Fotos\\', topdown=True):
+    if len(files) > 0:
+        res.extend(list(zip([root]*len(files), files)))
+
+df = pd.DataFrame(res, columns=['Path', 'File_Name'])
+
+
+df = df[df['File_Name'] != 'Thumbs.db']
+df['ClientId'] = df.Path.apply(lambda x: int(x.split("\\")[-1]))
+df = df[df['ClientId'] < 10000]
+
+df['Full_Path'] = df["Path"] + '\\' + df["File_Name"]
+df['Cat'] = df.File_Name.apply(lambda x: x.split(".")[0].split("_")[-1])
+
+df = df[df.Cat.isin(['1','3','4'])]
+
+from sklearn.model_selection import train_test_split
+
+df_train, df_test = train_test_split(df, test_size=0.2)
+
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 64, activation = 'relu'))
+classifier.add(Dense(units = 3, activation = 'softmax'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_dataframe(dataframe=df_train, directory = None, x_col='Full_Path', y_col='Cat',
+                                                 target_size = (64, 64),
+                                                 class_mode = 'categorical')
+
+test_set = train_datagen.flow_from_dataframe(dataframe=df_test, directory = None, x_col='Full_Path', y_col='Cat',
+                                                 target_size = (64, 64),
+                                                 class_mode = 'categorical')
+
+# training_set = train_datagen.flow_from_directory('Lab3/training_set',
+#                                                  target_size = (64, 64),
+#                                                  batch_size = 32,
+#                                                  class_mode = 'binary')
+#
+# test_set = test_datagen.flow_from_directory('Lab3/test_set',
+#                                             target_size = (64, 64),
+#                                             batch_size = 32,
+#                                             class_mode = 'binary')
+#
+training_set.class_indices
+
+hist=classifier.fit_generator(training_set,
+                         steps_per_epoch = 4000,
+                         epochs = 15,
+                         validation_data = test_set,
+                         validation_steps = 1000)
+
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
+
+Y_pred = classifier.predict_generator(test_set, 63) # num_of_test_samples // batch_size+1
+#Y_pred = (Y_pred>0.5)
+print('Confusion Matrix')
+print(confusion_matrix(test_set.classes, Y_pred))
+print('Classification Report')
+target_names = ['Kitchen', 'Bedroom', 'Bathroom']
+print(classification_report(test_set.classes, Y_pred, target_names=target_names))
