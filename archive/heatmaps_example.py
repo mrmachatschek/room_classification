8081c6dd0570eb55624e77f18833b13bc10adb1a
francisco.cruz <RUIFCRUZ@GMAIL.COM>
2020-06-06 15:29:58 +0100

    heatmaps example

---
+++ archive/heatmaps_example.py
@@ -0,0 +1,56 @@
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import keras.backend as K
+import numpy as np
+import cv2
+import sys
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+
+model = VGG16(weights="imagenet")
+img_path = 'E:\\011_Fotos\\1\\1_20_4.jpg'  #sys.argv[1]
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted: ', decode_predictions(preds, top=3)[0])
+class_idx = np.argmax(preds[0])
+class_output = model.output[:, class_idx]
+last_conv_layer = model.get_layer("block5_conv3")
+
+grads = K.gradients(class_output, last_conv_layer.output)[0]
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+pooled_grads_value, conv_layer_output_value = iterate([x])
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+heatmap = np.maximum(heatmap, 0)
+heatmap /= np.max(heatmap)
+
+img = cv2.imread(img_path)
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+heatmap = np.uint8(255 * heatmap)
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+
+cv2.imshow("Original", ResizeWithAspectRatio(img, width=800))
+cv2.imshow("GradCam", ResizeWithAspectRatio(superimposed_img, width=800) )
+cv2.waitKey(0)
